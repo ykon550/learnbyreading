e6030bcc8904e4b21464294c983d47c2e3175fc5
ya9do <moreisdifferent@gmail.com>
2018-10-21 14:57:37 -0700

    implmented export feature

--- public/js/popup.js
+++ public/js/popup.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     link.addEventListener('click', function () {
         openIndex();
     });
+
     const toggle = document.getElementById('stateOnOff');
     chrome.runtime.sendMessage({ messageType: "getState" }, function (res) {
         toggle.checked = res.state;
@@ -15,6 +16,17 @@ document.addEventListener('DOMContentLoaded', function () {
         const hasSetEnabled = this.checked;
         chrome.runtime.sendMessage({ messageType: "setState", hasSetEnabled: hasSetEnabled }, function (res) {
             console.log(res.reply);
+        });
+    });
+
+    const exportButton = document.getElementById('exportButton');
+    exportButton.addEventListener('click', function () {
+        chrome.runtime.sendMessage({ messageType: "getWords" }, function (res) {
+            const blob = new Blob([JSON.stringify(res.words)], { type: "application/json" });
+            const url = URL.createObjectURL(blob);
+            chrome.downloads.download({
+                url: url
+            });
         });
     });
 });
