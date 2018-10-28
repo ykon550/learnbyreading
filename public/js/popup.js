5f9ab8dadf9fa6cbe348c9c5c86f72251aca0aa3
ya9do <moreisdifferent@gmail.com>
2018-10-27 22:12:39 -0700

    changed format to CSV for export data

--- public/js/popup.js
+++ public/js/popup.js
@@ -2,6 +2,17 @@ const openIndex = () => {
     chrome.tabs.create({ "url": "index.html" });
 }
 
+const json2csv = (json) => {
+    const keys = Object.keys(json[0]);
+    const header = keys.map((key) => '"' + key + '"').join(', ') + '\n';
+    const body =  json.map(record => {
+        return keys.map((key) => {
+            return '"' + record[key] + '"';
+        }).join(', ');
+    }).join('\n');
+    return header + body;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const link = document.getElementById('wordList');
     link.addEventListener('click', function () {
@@ -22,7 +33,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const exportButton = document.getElementById('exportButton');
     exportButton.addEventListener('click', function () {
         chrome.runtime.sendMessage({ messageType: "getWords" }, function (res) {
-            const blob = new Blob([JSON.stringify(res.words)], { type: "application/json" });
+            const blob = new Blob([json2csv(res.words)], { type: "text/csv" });
+        //     const blob = new Blob([JSON.stringify(res.words)], { type: "application/json" });
             const url = URL.createObjectURL(blob);
             chrome.downloads.download({
                 url: url
